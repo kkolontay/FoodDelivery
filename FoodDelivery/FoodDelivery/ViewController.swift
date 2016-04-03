5ab45800fd9ea33c7822b75a3ab0aea83da30d68
kkolontay <k.kolontay@svitla.com>
2016-04-03 16:36:44 +0300

    first commit

---
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by kkolontay on 4/3/16.
+//  Copyright © 2016 kkolontay.test.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
