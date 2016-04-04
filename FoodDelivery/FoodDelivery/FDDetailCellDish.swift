d68de145b236bef7a4f3db130aba266a3adbd170
kkolontay <k.kolontay@svitla.com>
2016-04-04 10:26:50 +0300

    add second controller

---
+++ FoodDelivery/FoodDelivery/FDDetailCellDish.swift
@@ -0,0 +1,14 @@
+//
+//  FDDetailCellDish.swift
+//  FoodDelivery
+//
+//  Created by kkolontay on 4/4/16.
+//  Copyright © 2016 kkolontay.test.com. All rights reserved.
+//
+
+import UIKit
+
+
+class FDDetailCellDish: UITableViewCell {
+    
+}
---
+++ FoodDelivery/FoodDelivery/FDDetailList.swift
@@ -0,0 +1,29 @@
+//
+//  FDDetailList.swift
+//  FoodDelivery
+//
+//  Created by kkolontay on 4/4/16.
+//  Copyright © 2016 kkolontay.test.com. All rights reserved.
+//
+
+import UIKit
+
+
+class FDDetailListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.barTintColor = UIColor.redColor()
+        navigationController?.navigationBar.titleTextAttributes =
+            [NSForegroundColorAttributeName: UIColor.whiteColor(),  NSFontAttributeName: UIFont(name: "Arial", size: 27)!]
+        navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        navigationController?.navigationBar.topItem?.title = ""
+        self.title = "Takeaway menu"
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cellDish", forIndexPath: indexPath) as! FDDetailCellDish
+        return cell
+    }
+}
